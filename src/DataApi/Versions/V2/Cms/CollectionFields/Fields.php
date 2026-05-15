9aacec1bc1a5284bcbb90309112e1318ec3e8137
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2026-05-15 23:56:38 +0800

    Cleanup docstring

--- src/DataApi/Versions/V2/Cms/CollectionFields/Fields.php
+++ src/DataApi/Versions/V2/Cms/CollectionFields/Fields.php
@@ -28,10 +28,10 @@ class Fields extends CollectionFields
 
     /**
      * Create a custom field in a collection.
-       *
-       * Field validation is currently not available through the API.
-       *
-       * Bulk creation of fields is not supported with this endpoint. To add multiple fields at once, include them when you create the collection.
+     *
+     * Field validation is currently not available through the API.
+     *
+     * Bulk creation of fields is not supported with this endpoint. To add multiple fields at once, include them when you create the collection.
      *
      * @link https://developers.webflow.com/data/v2.0.0/reference/cms/collection-fields/create
      *
