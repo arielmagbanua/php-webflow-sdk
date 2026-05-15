d3a9bdb2acfd1f6cf19e04ec83138e104c1c1580
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2026-05-15 23:52:31 +0800

    Implemented CollectionFields

---
+++ src/DataApi/Cms/CollectionFields/Contracts/Fields.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ArielMagbanua\PhpWebflowApi\DataApi\Cms\CollectionFields\Contracts;
+
+use ArielMagbanua\PhpWebflowApi\DataApi\Api;
+
+/**
+ * The Fields contract for the Webflow API
+ *
+ * @package ArielMagbanua\PhpWebflowApi\DataApi\Cms\CollectionFields\Contracts
+ */
+abstract class Fields extends Api
+{
+    /**
+     * Constructor
+     *
+     * @param string $accessToken The access token
+     * @param string $version The version of the API
+     * @param string $collectionId The collection ID
+     */
+    public function __construct(
+        string $accessToken,
+        string $version,
+        protected string $collectionId,
+    ) {
+        parent::__construct($accessToken, $version);
+    }
+
+    /**
+     * Create a custom field in a collection.
+     *
+     * Field validation is currently not available through the API.
+     *
+     * Bulk creation of fields is not supported with this endpoint. To add multiple fields at once, include them when you create the collection.
+     *
+     * @param string $collectionId Unique identifier for a Collection
+     * @param array $field This can be static field, option field, or reference field
+     */
+    abstract public function createField(string $collectionId, array $field);
+}
---
+++ src/DataApi/Versions/V2/Cms/CollectionFields/Fields.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ArielMagbanua\PhpWebflowApi\DataApi\Versions\V2\Cms\CollectionFields;
+
+use ArielMagbanua\PhpWebflowApi\DataApi\Cms\CollectionFields\Contracts\Fields as CollectionFields;
+
+/**
+ * The Fields class for the Webflow API
+ *
+ * @package ArielMagbanua\PhpWebflowApi\DataApi\Versions\V2\Cms\CollectionFields
+ * @todo create unit tests for this class
+ */
+class Fields extends CollectionFields
+{
+    /**
+     * The Fields constructor
+     *
+     * @param string $accessToken The access token
+     * @param string $collectionId The collection ID
+     */
+    public function __construct(string $accessToken, string $collectionId)
+    {
+        // call the parent constructor
+        parent::__construct(accessToken: $accessToken, version: 'v2', collectionId: $collectionId);
+    }
+
+    /**
+     * Create a custom field in a collection.
+       *
+       * Field validation is currently not available through the API.
+       *
+       * Bulk creation of fields is not supported with this endpoint. To add multiple fields at once, include them when you create the collection.
+     *
+     * @link https://developers.webflow.com/data/v2.0.0/reference/cms/collection-fields/create
+     *
+     * @param string $collectionId The collection ID
+     * @param array $field The field to create
+     */
+    public function createField(string $collectionId, array $field)
+    {
+        return $this->sendRequest('POST', "collections/{$collectionId}/fields", $field);
+    }
+}
